e882c58d79c045892c1ff10271527f34af23cf74
dark0k_ <knezevicdarko899@gmail.com>
2023-01-20 10:35:00 +0100

    new site

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let inputCounter = document.getElementById("counter").textContent;
-let inputBox = document.querySelector("input");
+inputCounter = document.getElementById("counter");
+inputBox = document.querySelector("input");
 
 function changeCounter() {
-  inputCounter = inputBox.value.length.toString()
-  document.getElementById("counter").textContent = inputCounter + "/60"
+  inputCounter = inputBox.value.length;
+  document.getElementById("counter").textContent = inputCounter;
+
+  if (inputCounter >= 50) {
+    inputBox.className = "warning"
+    document.getElementById("counter").className = "warning"
+  }
+  else {
+    inputBox.className = ""
+    document.getElementById("counter").className = ""
+  }
 }
 
 inputBox.addEventListener("input", changeCounter);
